7c5c5bc044a8be4669a705f6c104858eb6f0e9d6
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-08 02:01:08 +0530

    roboshop-infra-test

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "test" {
-  source = "https://github.com/paulpremkumar241122/terraform-module-app.git"
+  source = "git::https://github.com/paulpremkumar241122/terraform-module-app.git"
   env = "dev"
 }
