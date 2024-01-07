8db6ee828b67f5621ff52a3e2b832b49f1a92da3
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-08 04:55:06 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-module "test" {
+module "instances" {
+  for_each = var.components
   source = "git::https://github.com/paulpremkumar241122/terraform-module-app.git"
+  component = each.key
   env = var.env
 }
