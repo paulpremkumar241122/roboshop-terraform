2427abd3b96f223bc56b42f4625bec9f468ace6f
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-20 03:42:11 +0530

    subnets with vpc

--- main.tf
+++ main.tf
@@ -2,17 +2,10 @@ module "vpc" {
   source = "git::https://github.com/paulpremkumar241122/terraform-module-vpc.git"
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
+  subnets = each.value["subnets"]
   env = var.env
   tags = var.tags
 }
-
-
-module "subnets" {
-  source = "./subnets"
-  for_each = var.subnets
-  cidr_block = element(var.cidr_block), count.index
-}
-
 
 
 
