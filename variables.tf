0d1a2537d6edb4a3bc675644c4b8c3e09df2cc7f
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-20 09:29:12 +0530

    Restart with subnet
    web - subnet

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "env" {}
 variable "tags" {}
 variable "vpc" {}
-variable "subnets" {}
-variable "cidr_block" {}
+#variable "subnets" {}
+#variable "cidr_block" {}
 #variable "components" {}
